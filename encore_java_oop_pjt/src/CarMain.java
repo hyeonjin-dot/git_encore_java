52d70112240251c1f5fd5386e7b1a8556f9a0f4c
hyeonjin-dot <74529390+hyeonjin-dot@users.noreply.github.com>
2024-01-29 16:35:29 +0900

    Update CarMain.java

--- encore_java_oop_pjt/src/CarMain.java
+++ encore_java_oop_pjt/src/CarMain.java
@@ -3,8 +3,8 @@ import com.encore.car.Car;
 public class CarMain {
 
 	public static void main(String[] args) {
-		Car car = new Car("가솔린");
-		
+		Car car = new Car();
+		//Car car = new Car("가솔린");
 		car.engineInfo();
 
 	}
